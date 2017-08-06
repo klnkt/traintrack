c54b5a976f1708dfafbdfa3eace7dd7f23ea2ef1
Николай Яшин <nyashin92@gmail.com>
2017-08-06 22:11:15 +0300

    added jest, added test for exercises reducer, updated package.json versions policy

---
+++ frontend/src/exercises/Reducers.test.js
@@ -0,0 +1,103 @@
+import reducer from './Reducers';
+import { actions } from './Actions';
+
+const emptyExercise = {
+  id: null,
+  properties: [],
+  title: '',
+};
+
+it('gets initial state', () => {
+  expect(reducer(undefined, {})).toEqual({
+    list: [],
+    edited: emptyExercise,
+  });
+});
+
+it('should handle ADD_EXERCISE', () => {
+  const newExercise = {
+    id: null,
+    title: 'foo',
+    properties: [],
+  };
+  expect(reducer({
+    list: [],
+    edited: newExercise,
+  }, {
+    type: actions.ADD_EXERCISE,
+  })).toEqual({
+    list: [{ ...newExercise, id: 1 }],
+    edited: newExercise,
+  });
+});
+
+it('should handle UPDATE_EXERCISE', () => {
+  const newExercise = {
+    id: 5,
+    title: 'foo',
+    properties: ['bar', 'baz'],
+  };
+  expect(reducer({
+    list: [{ id: 5 }, { id: 6 }, { id: 8 }],
+    edited: newExercise,
+  }, {
+    type: actions.UPDATE_EXERCISE,
+  })).toEqual({
+    list: [newExercise, { id: 6 }, { id: 8 }],
+    edited: newExercise,
+  });
+});
+
+it('should handle CHANGE', () => {
+  const newExercise = {
+    id: null,
+    title: 'foo',
+    description: 'baaar',
+    properties: ['bar', 'baz'],
+  };
+  expect(reducer({
+    list: [],
+    edited: newExercise,
+  }, {
+    type: actions.CHANGE,
+    changes: {
+      title: 'bar',
+      properties: ['fag, bag'],
+    },
+  })).toEqual({
+    list: [],
+    edited: {
+      id: null,
+      title: 'bar',
+      description: 'baaar',
+      properties: ['fag, bag'],
+    },
+  });
+});
+
+describe('should handle SET_EDITED_EXERCISE', () => {
+  it('should handle setting empty exercise', () => {
+    expect(reducer({
+      list: [],
+      edited: {},
+    }, {
+      type: actions.SET_EDITED_EXERCISE,
+      exerciseId: -1,
+    })).toEqual({
+      list: [],
+      edited: { ...emptyExercise, isNew: true },
+    });
+  });
+  it('should handle setting existing exercise', () => {
+    expect(reducer({
+      list: [{ id: 1, title: 'foo' }, { id: 2 }],
+      edited: {},
+    }, {
+      type: actions.SET_EDITED_EXERCISE,
+      exerciseId: 1,
+    })).toEqual({
+      list: [{ id: 1, title: 'foo', isNew: false }, { id: 2 }],
+      edited: { id: 1, title: 'foo', isNew: false },
+    });
+  });
+});
